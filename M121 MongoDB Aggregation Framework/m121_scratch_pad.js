fa43c46e15644201e78f0a4c051c9dfe0f973fae
Steve Curtis <steven.curtis@bt.com>
2020-11-07 21:01:46 +0000

    Added multi facet examples

--- M121 MongoDB Aggregation Framework/m121_scratch_pad.js
+++ M121 MongoDB Aggregation Framework/m121_scratch_pad.js
@@ -345,3 +345,37 @@ db.companies.aggregate([
     }
 ])
 
+
+//
+// $facet
+//
+db.companies.aggregate([
+    {
+        $match: { "$text": { "$search": "Databases" } }
+    },
+    {
+        $facet: {
+            "Categories": [{ "$sortByCount": "$category_code" }],
+            "Employees": [
+                { "$match": { "$founded_year": { "$gt": 1980 } } },
+                {
+                    "$bucket": {
+                        "groupBy": "$number_of_employees",
+                        "boundaries": [0, 20, 50, 100, 500, Infinity],
+                        "default": "Other"
+                    }
+                }
+            ],
+            "Founded": [
+                { "$match": { "offices.city": "New York" } },
+                {
+                    "$bucketAuto": {
+                        "groupBy": "$founded_year",
+                        "buckets": 5
+                    }
+                }
+            ]
+        }
+    }
+])
+
